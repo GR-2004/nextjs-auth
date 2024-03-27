4dcf1549e6aedcd436702a1c1d6b8e6c9dd4b5cb
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-03-27 14:12:12 +0530

    added login, logout and verify api

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,47 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { User } from "@/models/user.model"
+import { NextRequest, NextResponse } from "next/server"
+import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+connect()
+
+export async function POST(request: NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {email, password} = reqBody
+        //validation
+        console.log(reqBody);
+        const user = await User.findOne({email})
+
+        if(!user){
+            return NextResponse.json({error: "User not found"}, {status: 400})
+        }
+        
+        const validPassword = await bcryptjs.compareSync(password, user.password)
+        
+        if(!validPassword){
+            return NextResponse.json({error: "Incorrect password"}, {status: 400})
+        }
+
+        const tokenData = {
+            id: user._id
+        }
+
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: '1d'})
+
+        const response = NextResponse.json({
+            message: "Logged in successfully",
+            success: true
+        })
+
+        response.cookies.set("token", token, {
+            httpOnly: true
+        })
+
+        return response
+        
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
